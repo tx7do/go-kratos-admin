f15d039ca6920c8417bbd0c6e916b35c3619e468
Bobo <yanglinbo@gmail.com>
2025-04-01 21:49:47 +0800

    feat: refactor.

--- backend/pkg/service/name.go
+++ backend/pkg/service/name.go
@@ -1,5 +1,11 @@
 package service
 
+// 服务名称命名规则
+//
+// Consul：字母、数字、破折号；
+// Etcd:
+// Nacos:
+
 const (
-	AdminService = "kratos-admin.admin.gateway" // 后台BFF
+	AdminService = "kratosAdmin-admin-gateway" // 后台BFF
 )
